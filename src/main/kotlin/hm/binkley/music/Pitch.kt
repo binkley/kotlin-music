4cb9b67c02a782842bc778ef1dc1a781976a4db9
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-10-21 11:30:07 -0500

    Cleaner

--- src/main/kotlin/hm/binkley/music/Pitch.kt
+++ src/main/kotlin/hm/binkley/music/Pitch.kt
@@ -4,12 +4,14 @@ enum class Pitch {
     // Choosing the flattened form as # is not representable in code
     A, Bb, B, C, Db, D, Eb, E, F, Gb, G, Ab;
 
-    val dominant get() = values()[(ordinal + 7) % 12]
-    val subdominant get() = values()[(ordinal + 5) % 12]
+    val dominant get() = this + 7
+    val subdominant get() = this + 5
 
     /** The "relative minor" implies we are in a major key. */
-    val relativeMinor get() = values()[(ordinal + 9) % 12]
+    val relativeMinor get() = this + 9
 
     /** The "relative major" implies we are in a minor key. */
-    val relativeMajor get() = values()[(ordinal + 3) % 12]
+    val relativeMajor get() = this + 3
+
+    operator fun plus(steps: Int) = values()[(ordinal + steps) % 12]
 }
